b9b5335d132c08b323b50474ba8b9cf3e6f48eb9
Adam Fortuna <adam@fortuna.name>
2016-10-24 12:26:20 -0400

    Added sample solution

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/185856.json',
+    dataType: 'jsonp',
+    success: function(data) {
+      populateWithCourses(data.courses.completed)
+    }
+  });
+
+  function populateWithCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 
 });
